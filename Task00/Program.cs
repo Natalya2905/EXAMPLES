88e62c543e341573501b01e66b401674cbbaf7c3
Natalya2905 <natali.barkova.79@mail.ru>
2022-11-16 23:53:29 +0300

    Add Task00

---
+++ Task00/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число 
+// и выдаёт его квадрат
+
+// Например:
+// 4 -> 16
+// 3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+long n = Convert.ToInt64(Console.ReadLine());
+long square = n * n;
+
+Console.WriteLine ($"квадрат числа {n} = {square}");
---
+++ Task007/Program.cs
@@ -0,0 +1,16 @@
+﻿//7. Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает последнюю цифру
+//этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+Console.WriteLine ("Введите трёхзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 > 99 && number1 <1000)
+{
+Console.Write ("последняя цифра:  ");
+Console.WriteLine (number1 % 10);
+}
+
+else Console.WriteLine ("Введите трёхзначное число");
